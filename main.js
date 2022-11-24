f3aa0e607360498cc357244d5eaf37219feb7bab
ldrennan279 <leedrennan279@gmail.com>
2022-11-24 21:13:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 
-fetch("http://www.omdbapi.com/?t=batman&apikey=98442dff")
+fetch("http://www.omdbapi.com/?t=star&apikey=98442dff")
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    document.getElementById("title").textContent = `
+    document.getElementById("title").innerHTML = `
         ${data.Title}
         ${data.Year}
         ${data.Rated}
+        <img src="${data.Poster}"/>
     `
 })
-
-fetch("http://img.omdbapi.com/?t=batman&apikey=98442dff")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("title").innerHTML = `
-
-        `
-    })
